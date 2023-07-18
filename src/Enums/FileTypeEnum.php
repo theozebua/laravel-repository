b03a1d9b0539249f1a9a5c034eb83ff4b2595fb3
theozebua <ronizebua.rz@gmail.com>
2023-07-18 19:51:34 +0700

    `feat:` create enums

---
+++ src/Enums/FileTypeEnum.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Theozebua\LaravelRepository\Enums;
+
+use Illuminate\Support\Collection;
+
+enum FileTypeEnum: string
+{
+    case INTERFACE = 'Interface';
+    case REPOSITORY = 'Repository';
+
+    public static function list(): array
+    {
+        return Collection::make(self::cases())
+            ->map(fn (self $fileTypeEnum) => $fileTypeEnum->value)
+            ->toArray();
+    }
+}
---
+++ src/Enums/StubEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Theozebua\LaravelRepository\Enums;
+
+enum StubEnum: string
+{
+    case INTERFACE = 'interface.stub';
+    case INTERFACE_EXTENDS = 'interface.extends.stub';
+    case REPOSITORY = 'repository.stub';
+    case REPOSITORY_IMPLEMENTS = 'repository.implements.stub';
+}
