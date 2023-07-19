27e260231088fb425cd6f6a820771316687b8a9c
theozebua <ronizebua.rz@gmail.com>
2023-07-19 10:19:02 +0700

    `feat:` create repository generator

---
+++ src/RepositoryGenerator.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Theozebua\LaravelRepository;
+
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+use Illuminate\Support\Stringable;
+use Theozebua\LaravelRepository\Enums\StubEnum;
+
+final class RepositoryGenerator extends Generator
+{
+    use PathTrait;
+
+    protected bool $wantsToImplement;
+
+    public function __construct(
+        private Stringable|string|null $file = null,
+    ) {
+        parent::__construct();
+
+        $this->setFile($this->getRepositoryPath($file . '.php'));
+
+        $directory = $this->file()->replace('\\', '/')->beforeLast('/')->value();
+
+        if (!File::exists($directory)) {
+            File::makeDirectory($directory, recursive: true);
+        }
+    }
+
+    public function generate(bool $withInterfaces = false): void
+    {
+        $this->wantsToImplement = $withInterfaces;
+
+        $this->wantsToImplement
+            ? $this->withInterfaces()
+            : $this->withoutInterfaces();
+    }
+
+    public function destroy(): void
+    {
+        if (File::exists($this->file()->value())) {
+            File::delete($this->file()->value());
+        }
+    }
+
+    protected function withInterfaces(): void
+    {
+        //
+    }
+
+    protected function withoutInterfaces(): void
+    {
+        $this->setStub(StubEnum::REPOSITORY->value);
+
+        $fullyQualifiedClassName = $this->file()->className(
+            Config::get('laravel-repository.delimiter'),
+        );
+
+        $contents = Str::of(File::get($this->stub()))
+            ->replace(
+                [
+                    '{{ NAMESPACE }}',
+                    '{{ REPOSITORY }}',
+                ],
+                [
+                    $fullyQualifiedClassName
+                        ->beforeLast('\\')
+                        ->value(),
+                    $fullyQualifiedClassName
+                        ->afterLast('\\')
+                        ->value(),
+                ],
+            );
+
+        File::put($this->file()->value(), $contents);
+    }
+}
