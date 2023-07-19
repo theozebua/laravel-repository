105989024d51c49cfb91d9323f3d3481a9d92622
theozebua <ronizebua.rz@gmail.com>
2023-07-19 09:58:53 +0700

    `feat:` create `getInterfacePath` method

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Theozebua\LaravelRepository\Tests;
 
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Str;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 use Theozebua\LaravelRepository\InterfaceGenerator;
 use Theozebua\LaravelRepository\Support\File;
@@ -22,21 +24,26 @@ abstract class TestCase extends BaseTestCase
     protected function generateDummyInterfaces(): void
     {
         Collection::make($this->interfaces)->each(function (string $interface): void {
-            InterfaceGenerator::make($interface)
-                ->generate();
+            InterfaceGenerator::make($interface)->generate();
         });
     }
 
     protected function destroyDummyInterfaces(): void
     {
         Collection::make($this->interfaces)->each(function (string $interface): void {
-            InterfaceGenerator::make($interface)
-                ->destroy();
+            InterfaceGenerator::make($interface)->destroy();
         });
     }
 
     protected function existingInterfaces(): Collection
     {
         return File::existingInterfaces();
+    }
+
+    protected function getInterfacePath(string $path): string
+    {
+        return Config::get('laravel-repository.directories.interfaces')
+            . '/'
+            . Str::of($path)->trim('/');
     }
 }
