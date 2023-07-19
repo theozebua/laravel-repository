809c5c142e83fe56bf6668c7a87e4e881b3df10e
theozebua <ronizebua.rz@gmail.com>
2023-07-19 10:10:12 +0700

    `feat:` use `PathTrait`

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,14 +5,15 @@ declare(strict_types=1);
 namespace Theozebua\LaravelRepository\Tests;
 
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Str;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 use Theozebua\LaravelRepository\InterfaceGenerator;
+use Theozebua\LaravelRepository\PathTrait;
 use Theozebua\LaravelRepository\Support\File;
 
 abstract class TestCase extends BaseTestCase
 {
+    use PathTrait;
+
     protected $enablesPackageDiscoveries = true;
 
     protected array $interfaces = [
@@ -38,12 +39,5 @@ abstract class TestCase extends BaseTestCase
     protected function existingInterfaces(): Collection
     {
         return File::existingInterfaces();
-    }
-
-    protected function getInterfacePath(string $path = ''): string
-    {
-        return Config::get('laravel-repository.directories.interfaces')
-            . '/'
-            . Str::of($path)->trim('/');
     }
 }
